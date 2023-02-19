5f46713b742710b86dfc2f137ea25e42dcd7bde0
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2023-02-19 13:06:19 +0300

    AddAnimation

--- ITAcademyHT/Assets/_Scripts/CharacterController.cs
+++ ITAcademyHT/Assets/_Scripts/CharacterController.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
-public class CharacterController : MonoBehaviour
+public class Character : MonoBehaviour
 {
-    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] float _mass = 5;
     [SerializeField] private float jumpSpeed = 7;
-    [SerializeField] private float movementSpeed = 2.0f;
-    [SerializeField] private float sprintSpeed = 5.0f;
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private float sprintSpeed = 2;
     [SerializeField] private float rotationSpeed = 0.2f;
     [SerializeField] private float animationBlendSpeed = 0.2f;
 
-    private InputAction move;
-    private InputAction sprint;
-    private InputAction jump;
+    private InputAction _moveInput;
+    private InputAction _runInput;
+    private InputAction _jumpInput;
     private CharacterController controller;
     private Camera characterCamera;
     private Animator animator;
-    private PlayerControls playerControls;
+    private Controls _controller;
     private float rotationAngle = 0.0f;
     private float targetAnimationSpeed = 0.0f;
     private float currentSpeed;
 
     private bool isJumping = false;
-    private float speedY = 0.0f;
+    private float _vertSpeed = 0.0f;
 
     private Vector3 movement = Vector3.zero;
     private Vector3 rotatedMovement = Vector3.zero;
@@ -36,10 +38,10 @@ public class CharacterController : MonoBehaviour
 
     private void Awake()
     {
-        playerControls = new PlayerControls();
-        move = playerControls.Player.Move;
-        sprint = playerControls.Player.Sprint;
-        jump = playerControls.Player.Jump;
+        _controller = new Controls();
+        _moveInput = _controller.Controller.Movement;
+        _runInput = _controller.Controller.Run;
+        _jumpInput = _controller.Controller.Jump;
     }
 
     private void Start()
@@ -49,38 +51,31 @@ public class CharacterController : MonoBehaviour
 
     private void OnEnable()
     {
-        playerControls.Enable();
-        jump.started += Jump;
-        move.performed += Move;
-        move.canceled += StopMove;
-        sprint.performed += Sprint;
-        sprint.canceled += OffSprint;
+        _controller.Enable();
+        _jumpInput.performed += context => Jump();
+        _moveInput.performed += context => Move(_controller.Controller.Movement.ReadValue<Vector2>());
+        _moveInput.canceled += context => StopMove();
+        _runInput.performed += context => Sprint();
+        _runInput.canceled += context => OffSprint();
     }
 
     private void OnDisable()
     {
-        jump.started -= Jump;
-        sprint.canceled -= OffSprint;
-        sprint.performed -= Sprint;
-        move.performed -= Move;
-        move.canceled -= StopMove;
-        playerControls.Disable();
+        _jumpInput.performed -= context => Jump();
+        _moveInput.performed -= context => Move(_controller.Controller.Movement.ReadValue<Vector2>());
+        _moveInput.canceled -= context => StopMove();
+        _runInput.performed -= context => Sprint();
+        _runInput.canceled -= context => OffSprint();
+        _controller.Disable();
     }
 
     void Update()
     {
-        if (!Controller.isGrounded)
-        {
-            speedY += gravity * Time.deltaTime;
-        }
-        else if (speedY < -0.1f)
-        {
-            speedY = -0.1f;
-        }
-        Debug.Log(speedY);
-        Debug.Log(Controller.isGrounded);
-        CharacterAnimator.SetFloat("SpeedY", speedY / jumpSpeed);
-        if (isJumping && speedY < 0)
+        _vertSpeed += _gravity * _mass;
+        Vector2 inputDir = _controller.Controller.Movement.ReadValue<Vector2>();
+
+        //CharacterAnimator.SetFloat("SpeedY", _vertSpeed / jumpSpeed);
+        if (isJumping && _vertSpeed < 0)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f, LayerMask.GetMask("Default")))
@@ -90,64 +85,71 @@ public class CharacterController : MonoBehaviour
             }
         }
 
-        Vector3 verticalMovement = Vector3.up * speedY;
+        movement.x = inputDir.x;
+        movement.z = inputDir.y;
         rotatedMovement = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f) * movement;
+        //movement.y = _vertSpeed;
 
-        Controller.Move((verticalMovement + rotatedMovement * currentSpeed) * Time.deltaTime);
-        if (move.inProgress)
+        movement *= Time.deltaTime;
+        Controller.Move(CharacterCamera.transform.TransformDirection(movement));
+        if (_moveInput.inProgress)
         {
             rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
         }
 
-        CharacterAnimator.SetFloat("Speed", Mathf.Lerp(CharacterAnimator.GetFloat("Speed"), targetAnimationSpeed, animationBlendSpeed));
+        //CharacterAnimator.SetFloat("Speed", Mathf.Lerp(CharacterAnimator.GetFloat("Speed"), targetAnimationSpeed, animationBlendSpeed));
         Quaternion currentRotation = Controller.transform.rotation;
         Quaternion targetRotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
-        Controller.transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, rotationSpeed);
+        Controller.transform.localRotation = Quaternion.Lerp(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
-    private void Move(InputAction.CallbackContext obj)
+    private void Move(Vector2 dir)
     {
-        Vector2 moveDirection = obj.ReadValue<Vector2>();
-        movement.x = moveDirection.x;
-        movement.z = moveDirection.y;
-        targetAnimationSpeed = 0.5f;
+        CharacterAnimator.SetTrigger("Walk");
+        movement.x = dir.x;
+        movement.z = dir.y;
+        targetAnimationSpeed = 0.5f * currentSpeed;
     }
 
-    private void StopMove(InputAction.CallbackContext obj)
+    private void StopMove()
     {
+        CharacterAnimator.SetTrigger("Idle");
         movement = Vector3.zero;
         targetAnimationSpeed = 0.0f;
     }
 
-    private void Sprint(InputAction.CallbackContext obj)
+    private void Sprint()
     {
-        if (move.inProgress)
+        if (_moveInput.inProgress)
         {
+            CharacterAnimator.SetTrigger("Run");
             currentSpeed = sprintSpeed;
-            targetAnimationSpeed = 1.0f;
+            targetAnimationSpeed = 1.0f * currentSpeed;
         }
     }
 
-    private void OffSprint(InputAction.CallbackContext obj)
+    private void OffSprint()
     {
-        if (move.inProgress)
+        if (_moveInput.inProgress)
         {
+            CharacterAnimator.SetTrigger("Walk");
             currentSpeed = movementSpeed;
-            targetAnimationSpeed = 0.5f;
+            targetAnimationSpeed = 0.5f * currentSpeed;
         }
         else
         {
+            CharacterAnimator.SetTrigger("Idle");
             targetAnimationSpeed = 0.0f;
         }
     }
 
-    private void Jump(InputAction.CallbackContext obj)
+    private void Jump()
     {
         if (!isJumping)
         {
             isJumping = true;
             CharacterAnimator.SetTrigger("Jump");
-            speedY += jumpSpeed;
+            _vertSpeed += jumpSpeed;
         }
     }
 }
