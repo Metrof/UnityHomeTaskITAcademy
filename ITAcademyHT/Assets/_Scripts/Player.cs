2416c9def2c8c92958e9be64aceaadc6c3c210c7
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2023-03-13 12:07:30 +0300

    audio

--- ITAcademyHT/Assets/_Scripts/Player.cs
+++ ITAcademyHT/Assets/_Scripts/Player.cs
@@ -8,14 +8,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float _gravity = -9.81f;
     [SerializeField] float _speed = 5;
-    [SerializeField] float _rotationSpeed = 5;
 
-    private float _rotationAngle = 0.0f;
     private CharacterController _controller;
     private Camera _characterCamera;
     private Controller _inputController;
     private Vector2 _inputDir;
-    private Vector3 _rotatedMovement;
     public CharacterController Controller { get { return _controller ??= GetComponent<CharacterController>(); } }
     public Camera CharacterCamera { get { return _characterCamera ??= FindObjectOfType<Camera>(); } }
     private void Awake()
@@ -35,20 +32,10 @@ public class Player : MonoBehaviour
     private void Update()
     {
         _inputDir = _inputController.Player.Move.ReadValue<Vector2>();
-        _rotatedMovement = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f) * _inputDir;
-
         Vector3 movement = new(_inputDir.x * _speed, _gravity, _inputDir.y * _speed);
         movement *= Time.deltaTime;
-
         Controller.Move(transform.TransformDirection(movement));
 
-        if (_inputDir.sqrMagnitude > 0)
-        {
-            _rotationAngle = Mathf.Atan2(_rotatedMovement.x, _rotatedMovement.z) * Mathf.Rad2Deg;
-        }
-        Quaternion currentRotation = Controller.transform.rotation;
-        Quaternion targetRotation = Quaternion.Euler(0.0f, _rotationAngle, 0.0f);
-        print(targetRotation.y);
-        Controller.transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        Controller.transform.rotation = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f);
     }
 }
